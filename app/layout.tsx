a2f355b8dc299d1c336e244e506b5110a2003447
obaidraza022 <obaidraza772@gmail.com>
2025-07-28 09:45:59 +0530

    Implement ESLint & Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
