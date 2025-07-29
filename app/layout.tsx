765364d9b0c4142fa3ba4d2dcaee91f900f06c9d
obaidraza022 <obaidraza772@gmail.com>
2025-07-29 14:57:26 +0530

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import ThemeProvider from "@/context/Theme";
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import NavBar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -27,11 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
